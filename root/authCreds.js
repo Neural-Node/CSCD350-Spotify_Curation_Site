ad1ac52cfd8277fbd6695bcf6e8b059def731493
Jesse Javana <jjavana@ewu.edu>
2023-05-31 05:44:07 -0700

    Updated the Client ID and Client Secret to match dashboard

--- root/authCreds.js
+++ root/authCreds.js
@@ -11,8 +11,8 @@ const generateRandomString = () => {
 
 export const authCreds =
     {
-        client_id: 'your_client_id',
-        client_secret: 'your_client_secret',
-        redirect_uri: 'http://localhost:3000',
+        client_id: '37105f8a5dea4697bc0953aa8220272c',
+        client_secret: 'c82bd04155fc489f9078f1c9dbfd0831',
+        redirect_uri: 'http://localhost:63342/CSCD350-Spotify_Curation_Site/root/menu.html?_ijt=k5detbbhsmij5qn0h6phbuuir0&_ij_reload=RELOAD_ON_SAVE',
         auth_endpoint: 'https://accounts.spotify.com/authorize',
     }
